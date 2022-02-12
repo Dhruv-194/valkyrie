89311315dcaad791341e26d9528e9a278c927934
divyanshkul <k.divyansh268@gmail.com>
2022-02-12 17:44:03 +0530

    Added ERC marketplace constructors

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,22 @@
 require("@nomiclabs/hardhat-waffle");
-
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+const fs = require("fs");
+const projectId = fs.readFileSync(".mainNet").toString();
+const projectIDMumbai = fs.readFileSync(".polygonMumbai").toString();
+const privateKey = fs.readFileSync(".secret").toString();
 
 module.exports = {
+  networks: {
+    hardhat: {
+      chainID: 1337,
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${projectIDMumbai}`,
+      accounts: [privateKey],
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${projectId}`,
+      accounts: [privateKey],
+    },
+  },
   solidity: "0.8.4",
 };
