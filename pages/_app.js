8b2d53f3225421a6a1ccf9c125cabd35c542a46a
Dhruv <dhruv1592001@gmail.com>
2022-02-12 19:15:52 +0530

    Frontend part

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,38 @@
-import "../styles/globals.css";
-import { Link } from "react-router";
+import '../styles/globals.css'
+import Link from 'next/link'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
-      <nav className="border-b p-6">
-        <p className="text-4xl font-bold">asdasdasdasdasdasddasdasdasdasdasd</p>
-        <div className="flex mt-4 font-bold text-pink-700">asd</div>
+    <div>  
+      <nav className='border-b p-6'>
+        <p className='text-4xl font-bold'> Valkyrie : Superhero NFT marketplace </p>
+        <div className='flex mt-4'>
+        <Link href="/">
+            <a className="mr-6 text-red-500">
+              Home
+            </a>
+          </Link>
+          <Link href="/create-item">
+            <a className="mr-6 text-red-500">
+              Sell Superhero NFT
+            </a>
+          </Link>
+          <Link href="/my-assets">
+            <a className="mr-6 text-pink-500">
+              My Superhero NFTs
+            </a>
+          </Link>
+          <Link href="/creator-dashboard">
+            <a className="mr-6 text-pink-500">
+              My Sold Items
+            </a>
+          </Link>
+        </div>
       </nav>
-    </div>
-  );
+  <Component {...pageProps} />
+  </div>
+  )
+  
 }
 
-export default MyApp;
+export default MyApp
